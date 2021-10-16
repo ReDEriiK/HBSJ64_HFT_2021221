0fa6a5f8c9212a2d1028b3064eac77058fc259a5
CserfalviErik@stud.uni-obuda.hu <CserfalviErik@stud.uni-obuda.hu>
2021-10-16 10:23:42 +0200

    FilmDbContext was renamed and the database was extended

--- HBSJ64_HFT_2021221.Data/FilmDbContext.cs
+++ HBSJ64_HFT_2021221.Data/FilmDbContext.cs
@@ -4,12 +4,12 @@ using System;
 
 namespace HBSJ64_HFT_2021221.Data
 {
-    public class FilmContext : DbContext
+    public class FilmDbContext : DbContext
     {
         public virtual DbSet<Film> Films { get; set; }
         public virtual DbSet<Actor> Actors { get; set; }
         public virtual DbSet<Director> Directors { get; set; }
-        public FilmContext()
+        public FilmDbContext()
         {
             this.Database.EnsureCreated();
         }
@@ -23,14 +23,23 @@ namespace HBSJ64_HFT_2021221.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             Director d1 = new Director() { DirectorId = 1, Name = "Danny Boyle", Age = 64, Award = 20 };
+            Director d2 = new Director() { DirectorId = 2, Name = "George Lucas", Age = 77, Award = 60 };
+
 
             Film f1 = new Film() { FilmId = 1, Title = "Trainspotting", Genre = "Fekete komédia", DateOfPublish = 1996 };
+            Film f2 = new Film() { FilmId = 2, Title = "Star Wars I. rész – Baljós árnyak", Genre = "Akció", DateOfPublish = 1999 };
+            Film f3 = new Film() { FilmId = 3, Title = "Az elveszett frigyláda fosztogatói", Genre = "Akció", DateOfPublish = 1985 };
 
             Actor a1 = new Actor() { ActorId = 1, Name = "Ewan McGregor", Age = 50, Awards = 11 };
+            Actor a2 = new Actor() { ActorId = 2, Name = "Harrison Ford", Age = 79, Awards = 12};
+
 
             f1.DirectorId = d1.DirectorId;
             f1.MainActorId = a1.ActorId;
-
+            f2.DirectorId = d2.DirectorId;
+            f2.MainActorId = a1.ActorId;
+            f3.DirectorId = d2.DirectorId;
+            f3.MainActorId= a2.ActorId;
 
 
 
@@ -43,9 +52,9 @@ namespace HBSJ64_HFT_2021221.Data
                 entity.HasOne(film => film.Director).WithMany(director => director.Films).HasForeignKey(film => film.DirectorId).OnDelete(DeleteBehavior.ClientSetNull);
             });
 
-            modelBuilder.Entity<Film>().HasData(f1);
-            modelBuilder.Entity<Actor>().HasData(a1);
-            modelBuilder.Entity<Director>().HasData(d1);
+            modelBuilder.Entity<Film>().HasData(f1, f2, f3);
+            modelBuilder.Entity<Actor>().HasData(a1, a2);
+            modelBuilder.Entity<Director>().HasData(d1, d2);
 
         }
     }
