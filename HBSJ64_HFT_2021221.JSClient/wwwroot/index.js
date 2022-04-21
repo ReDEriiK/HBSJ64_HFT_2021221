7ee5e9d64ed71eaa1c020bb50272377577c9613d
CserfalviErik@stud.uni-obuda.hu <CserfalviErik@stud.uni-obuda.hu>
2022-04-21 15:59:39 +0200

    done

--- HBSJ64_HFT_2021221.JSClient/wwwroot/index.js
+++ HBSJ64_HFT_2021221.JSClient/wwwroot/index.js
@@ -1,23 +1,85 @@
 ﻿let films = [];
+let connection = null;
 
-fetch('http://localhost:4472/film')
-    .then(x => x.json())
-    .then(y => {
-        films = y;
-        console.log(films);
-        display();
+getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:4472/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+
+    connection.on("MovieCreated", (user, message) =>
+    {
+        console.log(user);
+        console.log(message);
     });
 
+    connection.onclose(async () =>
+    {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+
+async function getdata() {
+    await fetch('http://localhost:4472/film')
+        .then(x => x.json())
+        .then(y => {
+            films = y;
+            //console.log(films);
+            display();
+        });
+    
+}
+
+
+
+
 function display() {
+    document.getElementById('resultarea').innerHTML = "";
     films.forEach(t => { 
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.filmId + "</td><td>"
             + t.title + "</td><td>"
             + t.genre + "</td><td>"
-            + t.dateOfPublish + "</td></tr>";
+            + t.dateOfPublish + "</td><td>"
+            + `<button type="button" onclick="remove(${t.filmId})"> Delete</button>` + "</td></tr>";
     });
 
 }
+
+function remove(id) {
+    fetch('http://localhost:4472/film/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    
+}
+
 function create() {
     let titleofthefilm = document.getElementById('filmtitle').value;
     let genreofthefilm = document.getElementById('filmgenre').value;
@@ -45,4 +107,5 @@ function create() {
         .catch((error) => {
             console.error('Error:', error);
         });
+    getdata();
 }
